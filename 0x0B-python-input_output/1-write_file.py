ad7dd881502348d411667ee31baf544af6f4a758
dagemga4 <dagemga4@gmail.com>
2023-04-12 07:13:41 -0700

    again

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Module: 1-write_file
-This is a function that writes a string to a text file (UTF8) and returns the number of characters written
-"""
+# 3-write_file.py
+"""Defines a function that writes to a file."""
 
 
 def write_file(filename="", text=""):
-    """This function writes a string to a text file
+    """Write a string to a UTF8 text file.
     Args:
-        - filename: the name of the file that is to be write to
-        - text: the string to be added to the file
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
-
-    with open(filename, 'w+') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""Module: 7-add_item
-This is a script that adds all arguments to a Python list.
-Save them to a file.
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
 """
 
-
-import json
-import sys
-import os.path
-
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-new_file = "add_item.json"
-new_list = []
+filename = "add_item.json"
 
-if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
-    new_list = load_from_json_file(new_file)
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
 
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        new_list.append(elem)
+for arg in argv[1:]:
+    json_list.append(arg)
 
-save_to_json_file(new_list, new_file)
+save_to_json_file(json_list, filename)
